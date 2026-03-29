c9882ac0bd51087bf7f3474cb34ae716c90f76ae
ajayanjay <muhammad.azzar.tif424@polban.ac.id>
2026-03-29 14:57:53 +0700

    fix simple voice chat not showing their icon

--- src/main/java/fin/starhud/mixin/MixinInGameHUD.java
+++ src/main/java/fin/starhud/mixin/MixinInGameHUD.java
@@ -1,32 +1,17 @@
 package fin.starhud.mixin;
 
-import fin.starhud.Main;
 import fin.starhud.condition.HeldItemTooltip;
 import fin.starhud.condition.ScoreboardHUD;
-import fin.starhud.hud.HUDComponent;
-import fin.starhud.hud.HUDId;
-import fin.starhud.hud.implementation.statuseffect.NegativeEffectHUD;
-import fin.starhud.hud.implementation.statuseffect.PositiveEffectHUD;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
-import net.minecraft.client.render.RenderTickCounter;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = InGameHud.class, priority = 500)
 public class MixinInGameHUD {
-    // Mixin used to override vanilla effect HUD, I'm not sure whether this can be done using HUDElementRegistry
-    @Inject(at = @At("HEAD"), method = "renderStatusEffectOverlay", cancellable = true)
-    private void renderStatusEffectOverlay(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci) {
-        final PositiveEffectHUD POSITIVE_EFFECT_HUD = (PositiveEffectHUD) HUDComponent.getInstance().getHUD(HUDId.POSITIVE_EFFECT);
-        final NegativeEffectHUD NEGATIVE_EFFECT_HUD = (NegativeEffectHUD) HUDComponent.getInstance().getHUD(HUDId.NEGATIVE_EFFECT);
-        if ((!Main.settings.generalSettings.inGameSettings.disableHUDRendering) && (POSITIVE_EFFECT_HUD.shouldRender() || NEGATIVE_EFFECT_HUD.shouldRender())) ci.cancel();
-    }
 
     @Redirect(
             method = "renderScoreboardSidebar(Lnet/minecraft/client/gui/DrawContext;Lnet/minecraft/scoreboard/ScoreboardObjective;)V",
