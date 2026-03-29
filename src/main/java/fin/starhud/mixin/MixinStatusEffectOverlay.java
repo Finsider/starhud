d938bbb529da27013c4a9ee1b95a1d764b1e6a8d
ajayanjay <muhammad.azzar.tif424@polban.ac.id>
2026-03-29 15:28:25 +0700

    2.5.1

---
+++ src/main/java/fin/starhud/mixin/MixinStatusEffectOverlay.java
@@ -0,0 +1,29 @@
+package fin.starhud.mixin;
+
+import fin.starhud.Main;
+import fin.starhud.hud.HUDComponent;
+import fin.starhud.hud.HUDId;
+import fin.starhud.hud.implementation.statuseffect.NegativeEffectHUD;
+import fin.starhud.hud.implementation.statuseffect.PositiveEffectHUD;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.hud.InGameHud;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = InGameHud.class, priority = 2000)
+public class MixinStatusEffectOverlay {
+    @Unique
+    private static final PositiveEffectHUD POSITIVE_EFFECT_HUD = (PositiveEffectHUD) HUDComponent.getInstance().getHUD(HUDId.POSITIVE_EFFECT);
+
+    @Unique
+    private static final NegativeEffectHUD NEGATIVE_EFFECT_HUD = (NegativeEffectHUD) HUDComponent.getInstance().getHUD(HUDId.NEGATIVE_EFFECT);
+
+    // Mixin used to override vanilla effect HUD, I'm not sure whether this can be done using HUDElementRegistry
+    @Inject(at = @At("HEAD"), method = "renderStatusEffectOverlay", cancellable = true)
+    private void renderStatusEffectOverlay(DrawContext context, CallbackInfo ci) {
+        if ((!Main.settings.generalSettings.inGameSettings.disableHUDRendering) && (POSITIVE_EFFECT_HUD.shouldRender() || NEGATIVE_EFFECT_HUD.shouldRender())) ci.cancel();
+    }
+}
