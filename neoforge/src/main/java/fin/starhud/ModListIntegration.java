586b7e468d6c5b60cbca7efd5382fee274aaf6b3
finsider <muhammadazzar56@gmail.com>
2026-07-17 19:40:32 +0700

    multiloader port, neoforge support

---
+++ neoforge/src/main/java/fin/starhud/ModListIntegration.java
@@ -0,0 +1,18 @@
+package fin.starhud;
+
+import fin.starhud.config.Settings;
+import me.shedaniel.autoconfig.AutoConfigClient;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+
+public class ModListIntegration {
+
+    public static void registerModScreen(ModContainer container) {
+        container.registerExtensionPoint(
+                IConfigScreenFactory.class,
+                (client, parent) -> AutoConfigClient
+                        .getConfigScreen(Settings.class, parent)
+                        .get()
+        );
+    }
+}
